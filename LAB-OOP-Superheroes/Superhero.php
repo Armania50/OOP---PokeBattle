1fe0aa97db16400f169d4f95b0770696d6edc543
Armania50 <arya.00767@gmail.com>
2019-03-20 23:32:08 +0100

    LAB (oop)
    The smal before project for the LAB (oop- project)

---
+++ LAB-OOP-Superheroes/Superhero.php
@@ -0,0 +1,16 @@
+<?php
+
+class Superhero {
+public $name
+public $gender
+public $team
+public $oneline
+}
+
+public function __construct($name, $gender, $team, $oneliner)
+{
+        $this->name = $name;
+        $this->gender = $gender;
+        $this->team = $team;
+        $this->oneliner = $oneliner;
+}
---
+++ LAB-OOP-Superheroes/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require 'Superhero.php';
+
+$spiderman =  new Superhero('Spider-Man', 'Male', 'Spiderfriends', 'With great power comes great responsibility!');
+
+$spiderman =  new Superhero('Spider-Man', 'Male', 'Spiderfriends', 'With great power comes great responsibility!');
+
+print_r('<pre>'. $spiderman . '</pre>');
+ 
