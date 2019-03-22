8f4138550d28bc24c5a1d22d9d1162d2c0e10d1e
Armania50 <arya.00767@gmail.com>
2019-03-22 23:58:16 +0100

    Oefening LAB-Oop Refining the distinctions of the elements
    Oefening LAB-Oop Refining the distinctions of the elements.

---
+++ LAB-OOP-Superheroes/Avenger.php
@@ -0,0 +1,9 @@
+class Avenger extends Superhero {
+
+    public $team = 'Avengers';
+
+public function __construct($name, $gender, $oneliner)
+{
+    parent::__construct($name, $gender, $this->team, $oneliner);
+}
+}
--- LAB-OOP-Superheroes/index.php
+++ LAB-OOP-Superheroes/index.php
@@ -3,8 +3,11 @@
 require 'Superhero.php';
 
 $spiderman =  new Superhero('Spider-Man', 'Male', 'Spiderfriends', 'With great power comes great responsibility!');
-
-$spiderman =  new Superhero('Spider-Man', 'Male', 'Spiderfriends', 'With great power comes great responsibility!');
-
-print_r('<pre>'. $spiderman . '</pre>');
  
+ print_r('<pre>'. $spiderman . '</pre>');
+ $spiderman->sayOneliner();
+ 
+ $thor = new Avenger('Thor', 'Male', 'For Asgard!');
+ $thor->sayOneliner();
+
+print_r('<pre>' . $thor . '</pre>');
